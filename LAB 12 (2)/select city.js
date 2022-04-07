13af8adf24df39d972429050bdab7acbd3bd1f00
20it135 <80442908+shailshah2757@users.noreply.github.com>
2022-04-07 11:56:46 +0530

    Update select city.js

--- LAB 12 (2)/select city.js
+++ LAB 12 (2)/select city.js
@@ -9,22 +9,27 @@ selection.addEventListener('change', ()=>{
     if(city=="ahmedabad"){
         container.classList.remove("vadodara","surat","rajkot","junagadh")
         container.classList.add("ahmedabad");
+        alert("Ahmedabad is famous for Kankaria lake")
     }
     else if(city=="vadodara"){
         container.classList.remove("ahmedabad","surat","rajkot","junagadh")
         container.classList.add("vadodara");
+        alert("Vadodara is famous for MSU")
     }
     else if(city=="surat"){
         container.classList.remove("ahmedabad","vadodara","rajkot","junagadh")
         container.classList.add("surat");
+        alert("Surat is famous for 'Surati locho' ")
     }
     else if(city=="rajkot"){
         container.classList.remove("ahmedabad","vadodara","surat","junagadh")
         container.classList.add("rajkot");
+        alert("Rajkot is famous for Iron and forging industries")
     }
     else{
         container.classList.remove("ahmedabad","vadodara","surat","rajkot")
         container.classList.add("junagadh");
+        alert("Junagadh is famous for tourism")
     }
 })
 
